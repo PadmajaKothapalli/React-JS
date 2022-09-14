86e48928bab7c3c145612ca1e77d0d0202044faa
Padmaja <109962029+PadmajaKothapalli@users.noreply.github.com>
2022-09-14 15:32:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+class App extends React.Component{
+  render(){
+    return <div>
+      <h1>How to export the component?</h1>
+      <h2>1. Import a react component</h2>
+      <h2>2. Create a Class, it extends React.Component</h2>
+      <h2>3. Render the component</h2>
+      <h2>4. Export the component</h2>
+    </div>
+  }
+}
+export default App
